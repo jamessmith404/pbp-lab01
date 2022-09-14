7363f71fe465899f67da25a129dfc2de36a1ad6b
itsjamessw <james.smith@ui.ac.id>
2022-09-14 23:47:20 +0700

    add lab02 pbp updates

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,9 +1,13 @@
 # from lab01
 from django.urls import path 
-from wishlist.views import show_wishlist
+from wishlist.views import show_wishlist, show_wishlist_xml, show_wishlist_json #sesuaikan dengan nama fungsi yang dibuat
+from wishlist.views import show_json_by_id #sesuaikan dengan nama fungsi yang dibuat
 
 app_name = 'wishlist'
 
 urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
+    path('xml/', show_wishlist_xml, name="show_wishlist_xml"), #sesuaikan dengan nama fungsi yang dibuat
+    path('json/', show_wishlist_json, name="show_wishlist_json"), #sesuaikan dengan nama fungsi yang dibuat
+    path('json/<int:id>', show_json_by_id, name="show_json_by_id"), #sesuaikan dengan nama fungsi yang dibuat
 ]
