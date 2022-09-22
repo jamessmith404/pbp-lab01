c424be7f0e73950ddd14d033ee91d2bf6b4e6a49
itsjamessw <james.smith@ui.ac.id>
2022-09-22 13:38:08 +0700

    doing Lab 03 PBP

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -2,6 +2,11 @@
 from django.urls import path 
 from wishlist.views import show_wishlist, show_wishlist_xml, show_wishlist_json #sesuaikan dengan nama fungsi yang dibuat
 from wishlist.views import show_json_by_id #sesuaikan dengan nama fungsi yang dibuat
+from wishlist.views import register #sesuaikan dengan nama fungsi yang dibuat || Week 03
+from wishlist.views import login_user #sesuaikan dengan nama fungsi yang dibuat
+from wishlist.views import logout_user #sesuaikan dengan nama fungsi yang dibuat
+
+
 
 app_name = 'wishlist'
 
@@ -10,4 +15,7 @@ urlpatterns = [
     path('xml/', show_wishlist_xml, name="show_wishlist_xml"), #sesuaikan dengan nama fungsi yang dibuat
     path('json/', show_wishlist_json, name="show_wishlist_json"), #sesuaikan dengan nama fungsi yang dibuat
     path('json/<int:id>', show_json_by_id, name="show_json_by_id"), #sesuaikan dengan nama fungsi yang dibuat
+    path('register/', register, name='register'), #sesuaikan dengan nama fungsi yang dibuat || WEEK 03
+    path('login/', login_user, name='login'), #sesuaikan dengan nama fungsi yang dibuat
+    path('logout/', logout_user, name='logout'), #sesuaikan dengan nama fungsi yang dibuat
 ]
